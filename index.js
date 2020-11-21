7e6b092e393844009186d413c635d9956dbb960e
Sabbir Rifat <rifat234dgh@gmail.com>
2020-11-21 09:06:38 +0600

    MongoDB setup complete

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookiePareser = require('cookie-parser');
+
+const {User} = require('./model/user')
 
 mongoose.connect('mongodb+srv://sabbir:janina@cluster0.6movt.mongodb.net/test?retryWrites=true&w=majority', 
  { useUnifiedTopology: true, useNewUrlParser: true}).then(() => console.log('DB connected successfully')).catch((err) => console.error(err));
 
 
 
-app.get('/', (req, res) => {
-    res.send('hello Mara vhai')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodeyParser.json());
+app.user(cookiePareser());
+
+app.get('/api/users/register', (req, res) => {
+    return res.status(200)
 })
 
 app.listen(5000);
